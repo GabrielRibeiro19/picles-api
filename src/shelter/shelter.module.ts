3cc4ae031c17ea4c29160bb0412fef74bf221f31
GabrielRibeiro19 <gabriel.hsribeiro19@gmail.com>
2024-04-04 22:33:20 -0300

    Criação da conexão com o banco

--- src/shelter/shelter.module.ts
+++ src/shelter/shelter.module.ts
@@ -2,9 +2,15 @@ import { Module } from '@nestjs/common';
 import { ShelterController } from './shelter.controller';
 import ShelterTokens from './shelter.tokens';
 import GetShelterDetailsUseCase from './usecases/get.shelter.details.usecase';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Shelter, ShelterSchema } from './schemas/shelter.schema';
 
 @Module({
   controllers: [ShelterController],
+  imports: [
+    MongooseModule.forFeature([{ name: Shelter.name, schema: ShelterSchema }]),
+  ],
+
   providers: [
     {
       provide: ShelterTokens.getShelterDetailsUseCase,
@@ -12,4 +18,4 @@ import GetShelterDetailsUseCase from './usecases/get.shelter.details.usecase';
     }
   ],
 })
-export class ShelterModule {}
+export class ShelterModule { }
